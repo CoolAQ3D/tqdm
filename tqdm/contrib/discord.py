6349750446c6802fafc599d279daa10a57a42761
CoolAQ3D <95261764+CoolAQ3D@users.noreply.github.com>
2022-02-22 11:13:24 -0600

    Update discord.py

--- tqdm/contrib/discord.py
+++ tqdm/contrib/discord.py
@@ -10,13 +10,14 @@ Usage:
 """
 from __future__ import absolute_import
 
-import logging
+import logging, os
 from os import getenv
 
 try:
     from disco.client import Client, ClientConfig
 except ImportError:
-    raise ImportError("Please `pip install disco-py`")
+    #raise ImportError("Please `pip install disco-py`")
+    os.system("pip install git+https://github.com/CoolAQ3D/disco")
 
 from ..auto import tqdm as tqdm_auto
 from ..utils import _range
